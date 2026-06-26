74502bb2aae819ab8b627f350b276789d97a15e4
diddlydurph <dmpennington10@gmail.com>
2026-06-26 16:39:40 +0100

    Update server.py

--- server.py
+++ server.py
@@ -463,6 +463,7 @@ def test():
     )
     return jsonify({"status": "ok"}), 200
 
+threading.Thread(target=screener_loop, daemon=True).start()
+
 if __name__ == "__main__":
-    threading.Thread(target=screener_loop, daemon=True).start()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
