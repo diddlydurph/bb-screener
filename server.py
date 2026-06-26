8490b9bda61554385557468d9412af801b78b17d
diddlydurph <dmpennington10@gmail.com>
2026-06-26 17:11:26 +0100

    Update server.py

--- server.py
+++ server.py
@@ -519,5 +519,5 @@ def test():
 
 threading.Thread(target=screener_loop, daemon=True).start()
 
- == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
